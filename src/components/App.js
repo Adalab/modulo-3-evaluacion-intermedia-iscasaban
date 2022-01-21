c3ef2baa7cea7a3ac6f016c1e22639118fa04d12
Inma Sánchez Casabán <iscasaban@gmail.com>
2022-01-21 17:07:27 +0100

    add html form

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,49 @@ function App() {
           {htmlAdalabers}
         </tbody>
       </table>
+      <section>
+        <h2 className="subtitle">Añadir una Adalaber</h2>
+
+        <form action="" className="form__title">
+          <label className="form__text" htmlFor="name">
+            Nombre:
+            <input
+              type="text"
+              className="form__input"
+              //value={}
+              name="name"
+              id="id"
+              placeholder="Patatita"
+              //onChange={}
+            />
+          </label>
+          <label className="form__text" htmlFor="counselor">
+            Tutora:
+            <input
+              type="text"
+              className="form__input"
+              //value={}
+              name="name"
+              id="id"
+              placeholder="Maestro Jedi"
+              //onChange={}
+            />
+          </label>
+          <label className="form__text" htmlFor="specialty">
+            Especialidad:
+            <input
+              type="text"
+              className="form__input"
+              //value={}
+              name="name"
+              id="id"
+              placeholder="Tu superpoder"
+              //onChange={}
+            />
+          </label>
+          <button className="form__btn">Añadir una nueva Adalaber</button>
+        </form>
+      </section>
     </div>
   );
 }
